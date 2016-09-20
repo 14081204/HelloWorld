6ca659929a4b67710be10a04ac069ef5d2cfcb73
Lenovo-PC\lenovo <599318583@qq.com>
2016-09-20 08:19:32 +0800

    在node文件夹下实验了金字塔程序，目前可以输出给定好行数的金字塔

---
+++ node/main.js
@@ -0,0 +1,16 @@
+// TypeScript file  
+function pyramid(x) {
+    var s = "";
+    for (var b = 1; b <= x; b++) {
+        s = "";
+        for (var c = 0; c < x - b; c++) {
+            s = s + " ";
+        }
+        for (var d = 1; d <= b * 2 - 1; d++) {
+            s = s + "*";
+        }
+        console.log(s);
+    }
+}
+var y = 5;
+pyramid(y);
---
+++ node/main.ts
@@ -0,0 +1,16 @@
+// TypeScript file  
+function pyramid (x:number):void{
+    var s : string = "";
+    for(var b : number = 1; b <= x; b++){
+        s = "";        
+        for(var c : number = 0; c < x - b; c++){
+           s = s + " ";
+        }
+        for(var d : number = 1; d <= b * 2 - 1; d++){
+            s = s + "*";
+        }
+        console.log(s);
+    }
+}
+var y : number = 5;
+pyramid(y);
