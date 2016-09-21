6eb3c8f89f253c2c7dc2ec26ee39c5c8b96a7c77
Lenovo-PC\lenovo <599318583@qq.com>
2016-09-21 22:51:01 +0800

    Finish Homework 2

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('作业二测试');
+function Outputpyramid(totalLine) {
+    for (var currentLine = 1; currentLine <= totalLine; currentLine++) {
+        var singleLine = "";
+        for (var spaceCount = 0; spaceCount < totalLine - currentLine; spaceCount++) {
+            singleLine = singleLine + " ";
+        }
+        for (var starCount = 1; starCount <= currentLine * 2 - 1; starCount++) {
+            singleLine = singleLine + "*";
+        }
+        console.log(singleLine);
+    }
+}
+var cengshu = process.argv[2];
+Outputpyramid(parseInt(cengshu));
--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-console.log('作业二测试');
+function Outputpyramid (totalLine:number):void{
+    for(var currentLine : number = 1; currentLine <= totalLine; currentLine ++){
+        var singleLine = "";        
+        for(var spaceCount : number = 0; spaceCount < totalLine - currentLine; spaceCount++){
+           singleLine = singleLine + " ";
+        }
+        for(var starCount : number = 1; starCount <= currentLine * 2 - 1; starCount++){
+            singleLine = singleLine + "*";
+        }
+        console.log(singleLine);
+    }
+}
+
+var cengshu : string = process.argv[2];
+Outputpyramid(parseInt(cengshu));
